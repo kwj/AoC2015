7bcc91e5bdb1f753036a213f122712e103a52c06
kwj <kwj@vlax.net>
2025-01-15 12:44:41 -0800

    Add a shell script

---
+++ solve.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+    echo "usage: solve.sh <problem_number>"
+    exit 1
+fi
+
+sol=$(printf "day%02d" $1)
+
+folder="./src/${sol}"
+config="Release"
+runtime="net9.0"
+exe="${folder}/bin/${config}/${runtime}/${sol}"
+
+if [ ! -x ${exe} ]; then
+   dotnet build ${folder}/${sol}.fsproj -c Release --no-restore
+fi
+
+${exe} ${folder}/input 
+
+echo
