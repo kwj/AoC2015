af057470ab9018d3c155142614b12894dd15f59c
kwj <kwj@vlax.net>
2025-01-16 07:02:31 -0800

    Use Seq.findIndex

--- src/day04/Program.fs
+++ src/day04/Program.fs
@@ -3,19 +3,19 @@
 open System.Security.Cryptography
 open System.Text
 
-let partOne (secret: string) =
+let makeHashSeq (secret: string) =
     let md5 = MD5.Create()
+
     Seq.initInfinite string
-    |> Seq.find (fun s ->
-        let hash = md5.ComputeHash(Encoding.ASCII.GetBytes(secret + s))
-        hash[0] = 0uy && hash[1] = 0uy && hash[2] < 0x10uy)
+    |> Seq.map (fun s -> md5.ComputeHash(Encoding.ASCII.GetBytes(secret + s)))
+
+let partOne (secret: string) =
+    makeHashSeq secret
+    |> Seq.findIndex (fun hash -> hash[0] = 0uy && hash[1] = 0uy && (hash[2] &&& 0xF0uy = 0uy))
 
 let partTwo (secret: string) =
-    let md5 = MD5.Create()
-    Seq.initInfinite string
-    |> Seq.find (fun s ->
-        let hash = md5.ComputeHash(Encoding.ASCII.GetBytes(secret + s))
-        hash[0] = 0uy && hash[1] = 0uy && hash[2] = 0uy)
+    makeHashSeq secret
+    |> Seq.findIndex (fun hash -> hash[0] = 0uy && hash[1] = 0uy && hash[2] = 0uy)
 
 module Main =
     open System.IO
@@ -24,6 +24,6 @@ module Main =
     let main args =
         assert (Array.length args = 1)
         let secret = File.ReadAllText(args[0]).TrimEnd()
-        partOne secret |> printfn "%s"
-        partTwo secret |> printfn "%s"
+        partOne secret |> printfn "%d"
+        partTwo secret |> printfn "%d"
         0
